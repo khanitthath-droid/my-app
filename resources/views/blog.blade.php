9ff7ff4fae1d5edcf80c5a774f53d041e7a497f4
khanitthath-droid <khanittha.th@rmuti.ac.th>
2026-06-29 16:25:18 +0700

    อัพเดท

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ยินดีต้อนรับเข้าสุ่เว็บไซต์ของฉัน</title>
+    <title>เกี่ยวกับเรา</title>
 </head>
 <body>
-     <h2>ยินดีต้อนรับเข้าสุ่เว็บไซต์ของฉัน</h2>
+     <h2>เกี่ยวกับเรา</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta nihil, totam recusandae ex quis ab expedita sed nobis suscipit beatae autem alias voluptatibus a maiores non cupiditate, odit adipisci dolore.</p>
-    <a href="/about">About</a>
-    <a href="/blog">blog</a>
+    <a href="/">หน้าเว็บ</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("welcome");
+});
+
+Route::get('/about', function () {
+    return view("about");
+});
+
+Route::get('/blog', function () {
+    return view("blog");
 });
